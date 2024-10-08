cee690cbd4b767c7cb6081bf2890c56e8aabef1c
Maxim Sharenkov <mcshaman@gmail.com>
2024-10-08 15:54:41 +0300

    Update

--- components/vl53l0x_m/sensor.py
+++ components/vl53l0x_m/sensor.py
@@ -1,6 +1,6 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.components import i2c, vl5310x
+from esphome.components import i2c, sensor
 from esphome.const import (
     STATE_CLASS_MEASUREMENT,
     UNIT_METER,
@@ -11,18 +11,54 @@ from esphome.const import (
 )
 from esphome import pins
 
-DEPENDENCIES = ["i2c", "vl5310x"]
+DEPENDENCIES = ["i2c"]
 
-vl53l0x_m_ns = cg.esphome_ns.namespace("vl53l0x_m")
-VL53L0XMSensor = vl53l0x_m_ns.class_(
-    "VL53L0XMSensor", vl5310x, cg.PollingComponent, i2c.I2CDevice
+vl53l0x_ns = cg.esphome_ns.namespace("vl53l0x")
+VL53L0XSensor = vl53l0x_ns.class_(
+    "VL53L0XSensor", sensor.Sensor, cg.PollingComponent, i2c.I2CDevice
 )
+
+CONF_SIGNAL_RATE_LIMIT = "signal_rate_limit"
+CONF_LONG_RANGE = "long_range"
+
+
+def check_keys(obj):
+    if obj[CONF_ADDRESS] != 0x29 and CONF_ENABLE_PIN not in obj:
+        msg = "Address other then 0x29 requires enable_pin definition to allow sensor\r"
+        msg += "re-addressing. Also if you have more then one VL53 device on the same\r"
+        msg += "i2c bus, then all VL53 devices must have enable_pin defined."
+        raise cv.Invalid(msg)
+    return obj
+
+
+def check_timeout(value):
+    value = cv.positive_time_period_microseconds(value)
+    if value.total_seconds > 60:
+        raise cv.Invalid("Maximum timeout can not be greater then 60 seconds")
+    return value
+
 
 CONFIG_SCHEMA = cv.All(
     sensor.sensor_schema(
-        VL53L0XMSensor
+        VL53L0XSensor,
+        unit_of_measurement=UNIT_METER,
+        icon=ICON_ARROW_EXPAND_VERTICAL,
+        accuracy_decimals=2,
+        state_class=STATE_CLASS_MEASUREMENT,
     )
-    .extend(sensor.VL53L0XSensor)
+    .extend(
+        {
+            cv.Optional(CONF_SIGNAL_RATE_LIMIT, default=0.25): cv.float_range(
+                min=0.0, max=512.0, min_included=False, max_included=False
+            ),
+            cv.Optional(CONF_LONG_RANGE, default=False): cv.boolean,
+            cv.Optional(CONF_TIMEOUT, default="10ms"): check_timeout,
+            cv.Optional(CONF_ENABLE_PIN): pins.gpio_output_pin_schema,
+        }
+    )
+    .extend(cv.polling_component_schema("60s"))
+    .extend(i2c.i2c_device_schema(0x29)),
+    check_keys,
 )
 
 
