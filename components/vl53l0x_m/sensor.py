1bc4912db02dd3ea764f7c2a771dd04796536a41
Maxim Sharenkov <mcshaman@gmail.com>
2024-10-08 15:48:03 +0300

    Update

--- components/vl53l0x_m/sensor.py
+++ components/vl53l0x_m/sensor.py
@@ -1,6 +1,6 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.components import i2c, sensor
+from esphome.components import i2c, vl5310x
 from esphome.const import (
     STATE_CLASS_MEASUREMENT,
     UNIT_METER,
