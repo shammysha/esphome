b79538ce1308bd6a88c21459537f60bf45e118b8
shammysha <65885873+shammysha@users.noreply.github.com>
2025-01-26 22:15:23 +0300

    Update nartis-100.cpp

--- components/nartis100/nartis-100.cpp
+++ components/nartis100/nartis-100.cpp
@@ -390,7 +390,8 @@ int CommandDisconnect::fill_request(package_t *raw_package) {
 bool CommandGetSerialNumber::process_result(header_t *header, result_package_t *package) {
   uint8_t *ptr = package->buff;
   type_digit_t *unsigned32;
-  if ((*ptr++ == LSAP) && (*ptr++ == RESP_LSAP) && *ptr++ == 0 && *ptr == GET_RESPONSE && (unsigned32 = (type_digit_t*)(ptr + 4))->type == TYPE_UNSIGNED_32) {
+/*  if ((*ptr++ == LSAP) && (*ptr++ == RESP_LSAP) && *ptr++ == 0 && *ptr == GET_RESPONSE && (unsigned32 = (type_digit_t*)(ptr + 4))->type == TYPE_UNSIGNED_32) { */
+  if ((*ptr++ == LSAP) && (*ptr++ == RESP_LSAP) && *ptr++ == 0 && (unsigned32 = (type_digit_t*)(ptr + 4))->type == TYPE_UNSIGNED_32) {       
     number = reverse32(unsigned32->value);
     return true;
   }
