920b2d478fa5d5624f818016c3d701a6e85befd6
shammysha <65885873+shammysha@users.noreply.github.com>
2025-01-26 22:41:28 +0300

    Update nartis-100.cpp

--- components/nartis100/nartis-100.cpp
+++ components/nartis100/nartis-100.cpp
@@ -354,8 +354,6 @@ bool CommandOpenSession::process_result(header_t *header, result_package_t *pack
   uint8_t *ptr = package->buff;
   uint8_t aare_len;
   if ((*ptr++ == LSAP) && (*ptr++ == RESP_LSAP) && *ptr++ == 0 && (*ptr++ == AARE) && (aare_len = *ptr) > 0)
-    return true;
-     
     for(uint8_t i = 0; i < aare_len; i++) {
       if (*ptr++ == 0xA2) {
         ptr += *ptr;
@@ -390,9 +388,7 @@ int CommandDisconnect::fill_request(package_t *raw_package) {
 bool CommandGetSerialNumber::process_result(header_t *header, result_package_t *package) {
   uint8_t *ptr = package->buff;
   type_digit_t *unsigned32;
-/*  if ((*ptr++ == LSAP) && (*ptr++ == RESP_LSAP) && *ptr++ == 0 && *ptr == GET_RESPONSE && (unsigned32 = (type_digit_t*)(ptr + 4))->type == TYPE_UNSIGNED_32) { */
-  if ((*ptr++ == LSAP) && (*ptr++ == RESP_LSAP) && *ptr++ == 0) {       
-    unsigned32 = (type_digit_t*)(ptr + 4);
+  if ((*ptr++ == LSAP) && (*ptr++ == RESP_LSAP) && *ptr++ == 0 && *ptr == GET_RESPONSE && (unsigned32 = (type_digit_t*)(ptr + 4))->type == TYPE_UNSIGNED_32) { 
     number = reverse32(unsigned32->value);
     return true;
   }
